deeccc6ef3f75b3d9739fc7dbe7a7d16db3ee4b2
Sajith K <sajith@insidestorybox.com>
2020-07-10 18:37:56 +0530

    Updated

--- lamp.sh
+++ lamp.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt install apache2
-sudo apt install mysql-server
-sudo mysql_secure_installation
-sudo apt install php libapache2-mod-php php-mysql
-sudo apt install php-mbstring
-sudo apt install php-curl
-sudo apt install php-zip
-sudo apt install php-dom
-sudo apt install php-simplexml
-sudo apt install composer
-sudo apt install nodejs npm
+sudo apt install apache2 -y
+sudo apt install mysql-server -y
+sudo mysql_secure_installation -y
+sudo apt install php libapache2-mod-php php-mysql -y
+sudo apt install php-mbstring -y
+sudo apt install php-curl -y
+sudo apt install php-zip -y
+sudo apt install php-dom -y
+sudo apt install php-simplexml -y
+sudo apt install composer -y
+sudo apt install nodejs npm -y
 sudo service apache2 restart
+
+#wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip
+#
+
+
+#sudo mysql -p -u root
+#CREATE USER 'pmauser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password_here';
+#GRANT ALL PRIVILEGES ON *.* TO 'pmauser'@'localhost';
+
+#sudo mysql -p -u root
+#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'enter_password_here';
+#FLUSH PRIVILEGES;
+
+# <Directory /var/www/html/production>
+#         Options Indexes FollowSymLinks MultiViews
+#         AllowOverride All
+#         Require all granted
+#     </Directory>
+# </VirtualHost>
